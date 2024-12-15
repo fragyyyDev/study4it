94946ac43dc8021f110e69f3e9f39e04e2038aaa
fragyyyDeveloper <fragyyyDev@gmail.com>
2024-12-15 19:17:15 +0100

    Opraveny chyby se sidebarem

--- client/src/pages/HistoryTests.jsx
+++ client/src/pages/HistoryTests.jsx
@@ -10,8 +10,8 @@ const HistoryTests = () => {
       <Sidebar onVisibilityChange={setSidebarVisible} />
       <div
         className={`flex-1 transition-all duration-300 p-4 md:p-6 lg:p-8 xl:p-12 overflow-y-auto mt-12 ${isSidebarVisible
-            ? "ml-[100vw] md:ml-[20vw] sm:ml-[30vw] lg:ml-[17.5vw] xl:ml-[17vw] 2xl:ml-[12vw]"
-            : "ml-0"
+          ? "ml-[100vw] md:ml-[30vw] sm:ml-[35vw] lg:ml-[25vw] xl:ml-[22vw] 2xl:ml-[17vw]"
+          : "ml-0"
           }`}
       >
         <History />
--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,24 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar';
 import SettingsScreen from '../components/SettingsScreen';
 
 
 const Settings = () => {
-    const [isSidebarVisible, setSidebarVisible] = useState(true);
+  const [isSidebarVisible, setSidebarVisible] = useState(true);
 
-    return (
-      <div className="flex h-screen w-screen overflow-x-hidden">
-        <Sidebar onVisibilityChange={setSidebarVisible} />
-        <div
-          className={`flex-1 transition-all duration-300 p-4 md:p-6 lg:p-8 xl:p-12 overflow-y-auto mt-12 ${isSidebarVisible
-              ? "ml-[100vw] md:ml-[20vw] sm:ml-[30vw] lg:ml-[17.5vw] xl:ml-[17vw] 2xl:ml-[12vw]"
-              : "ml-0"
-            }`}
-        >
-          <SettingsScreen />
-        </div>
+  return (
+    <div className="flex h-screen w-screen overflow-x-hidden">
+      <Sidebar onVisibilityChange={setSidebarVisible} />
+      <div
+        className={`flex-1 transition-all duration-300 p-4 md:p-6 lg:p-8 xl:p-12 overflow-y-auto mt-12 ${isSidebarVisible
+          ? "ml-[100vw] md:ml-[30vw] sm:ml-[35vw] lg:ml-[25vw] xl:ml-[22vw] 2xl:ml-[17vw]"
+          : "ml-0"
+          }`}
+      >
+        <SettingsScreen />
       </div>
-    );
+    </div>
+  );
 }
 
 export default Settings
