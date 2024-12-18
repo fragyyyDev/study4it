e3535038b939ada97e35d27d6511448842a0f80e
fragyyyDeveloper <fragyyyDev@gmail.com>
2024-12-18 21:52:04 +0100

    Made information about tests better

--- client/src/components/InformationTestScreen.jsx
+++ client/src/components/InformationTestScreen.jsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { ArrowCircleRight, Warning, XCircle } from '@phosphor-icons/react'
 
 const InformationTestScreen = () => {
     const navigate = useNavigate();
@@ -29,32 +30,38 @@ const InformationTestScreen = () => {
 
     return (
         <div>
-            {existingSession && (
-                <div className="w-full h-24 flex bg-[#F1EAFF] items-center justify-between p-6">
-                    <p className='text-xl inter'>Vypadá to, že již máš rozdělaný test pojď ho dokončit !</p>
-                    <button className='inter bg-[#7263FF] p-2 rounded-xl text-white' onClick={() => {navigate("/psani-testu-nanecisto")}}>Psát test</button>
-                </div>
-            )}
             <h2 className='text-3xl inter font-semibold'>Simulace nanečisto</h2>
             <h3 className='text-xl inter'>Přijímací zkoušky</h3>
             <div className="w-full h-[1px] bg-[#F1EAFF] my-6"></div>
+            {existingSession && (
+                <div className="w-full h-20 flex bg-[#F1EAFF] items-center px-4 mb-3 rounded-xl gap-x-4">
+                    <Warning size={32} className='mx-4'/>
+                    <div className="flex justify-between w-full">
+                        <div className="">
+                            <p className='inter font-semibold'>Běží ti test! Pojď ho dokončit!</p>
+                            <p className='inter text-gray-500 text-sm'>Vypadá to , že jsi opustil svůj test. Neboj, jeho stav máš uložený a furt se k němu můžeš vrátit.</p>
+                        </div>
+                        <button className='inter bg-[#7263FF] p-2 rounded-xl text-white flex gap-x-2 items-center' onClick={() => { navigate("/psani-testu-nanecisto") }}>Pokračovat <ArrowCircleRight size={24} /></button>
+                    </div>
+                </div>
+            )}
             <div className="">
                 <p className='inter font-semibold'>Informace o testu</p>
                 <p className='inter text-gray-500'>Jelikož je test předdefinovaný nelze změnit jeho vlastnosti</p>
                 <div className="flex flex-col md:flex-row mt-4 md:gap-y-0 md:gap-x-2 gap-y-2">
-                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F1EAFF] rounded-lg flex flex-col gap-y-2">
+                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F8F6FF] rounded-lg flex flex-col gap-y-2">
                         <p className='inter text-gray-500 text-sm'>Časomíra</p>
                         <p className='font-semibold inter text-lg'>130 minut</p>
                     </div>
-                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F1EAFF] rounded-lg flex flex-col gap-y-2">
+                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F8F6FF] rounded-lg flex flex-col gap-y-2">
                         <p className='inter text-gray-500 text-sm'>Předmět(y)</p>
                         <p className='font-semibold inter text-lg'>Čeština, Matematika</p>
                     </div>
-                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F1EAFF] rounded-lg flex flex-col gap-y-2">
+                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F8F6FF] rounded-lg flex flex-col gap-y-2">
                         <p className='inter text-gray-500 text-sm'>Probíraná látka</p>
                         <p className='font-semibold inter text-lg'>Vše</p>
                     </div>
-                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F1EAFF] rounded-lg flex flex-col gap-y-2">
+                    <div className="md:w-1/4 w-full h-24 p-4 bg-[#F8F6FF] rounded-lg flex flex-col gap-y-2">
                         <p className='inter text-gray-500 text-sm'>Možnost upravit odpověď</p>
                         <p className='font-semibold inter text-lg'>Ne</p>
                     </div>
@@ -62,7 +69,7 @@ const InformationTestScreen = () => {
                 <div className="mt-4 flex flex-col gap-y-3">
                     <p className='inter font-semibold'>Další informace</p>
                     <div className="flex flex-col md:flex-row md:gap-x-2 md:gap-y-0 gap-y-2">
-                        <div className="w-full md:w-1/2 p-4 bg-[#F1EAFF] rounded-lg flex flex-col gap-y-2 pb-16">
+                        <div className="w-full md:w-1/2 p-4 bg-[#F8F6FF] rounded-lg flex flex-col gap-y-2 pb-16">
                             <p className='text-gray-500 inter'>Pravidla Testu</p>
                             <ul className="inter font-medium list-disc list-inside text-sm ml-4 text-dark tracking-[-0.01em]">
                                 <li>Časomíra 130 min celkem (bez stopování)</li>
@@ -71,7 +78,7 @@ const InformationTestScreen = () => {
                                 <li>Úlohy různého typu, délky a obtížnosti</li>
                             </ul>
                         </div>
-                        <div className="w-full md:w-1/2 p-4 bg-[#F1EAFF] rounded-lg flex flex-col gap-y-2">
+                        <div className="w-full md:w-1/2 p-4 bg-[#F8F6FF] rounded-lg flex flex-col gap-y-2">
                             <p className='text-gray-500 inter'>Jak postupovat v testu</p>
                             <ul className="inter font-medium list-disc list-inside text-sm ml-4 text-dark tracking-[-0.01em]">
                                 <li>V testu můžeš dát další/zpět aby jsi pokračoval nebo se vrátil</li>
@@ -80,7 +87,18 @@ const InformationTestScreen = () => {
                         </div>
                     </div>
                 </div>
-                <button onClick={() => startQuiz()}>Spustit test Nanečisto</button>
+                <div className="flex gap-x-2">
+                    <button onClick={() => startQuiz()}
+                        className='bg-[#7263FF] py-2.5 px-3 text-white inter flex gap-x-2 rounded-xl'
+                    >Spustit test
+                        <ArrowCircleRight size={24} />
+                    </button>
+                    <button onClick={() => navigate("/testy")}
+                        className='bg-[#141417] py-2.5 px-3 text-white inter flex gap-x-2 rounded-xl'
+                    >Přejít zpět do výběru
+                        <XCircle size={24} />
+                    </button>
+                </div>
             </div>
         </div>
     )
